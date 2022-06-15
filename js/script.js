390ed63aa0a0c97034a78f09286e4bc7a3896364
amaljom <amaljom99@gmail.com>
2022-06-15 16:44:47 +0200

    prendo e assegno gli input ricevuti dall utente

--- js/script.js
+++ js/script.js
@@ -1,25 +1,40 @@
 
+const dataAcquisition = document.querySelector('#data-acquisition');
 
-const pricePerKm= 0.26;
-let totalPrice= pricePerKm;
-console.log(totalPrice)
-totalPrice= totalPrice * userDistance;
+dataAcquisition.addEventListener('click', function(){
+    // ! faccio in modo che i valori inseriti dall' utente nel form vengano //
+    // ! assegnati alle variabili che userò per fare i calcoli //
+    let userAge = document.getElementById('user-age').value;
+    let userDistance = document.getElementById('user-km').value;
+    console.log(userAge, userDistance);
+});
 
-if( userAge < 18){
-    const discount= ((totalPrice * 15) / 100);
-    totalPrice=totalPrice-discount;
+
+
+// * const userAge=parseInt( prompt('inserisci la tua età') );
+// * const userDistance=parseInt( prompt('inserisci quanti km vuoi percorrere') );
+// * console.log(userAge, userDistance);
+
+// const pricePerKm= 0.26;
+// let totalPrice= pricePerKm;
+// console.log(totalPrice)
+// totalPrice= totalPrice * userDistance;
+
+// if( userAge < 18){
+//     const discount= ((totalPrice * 15) / 100);
+//     totalPrice=totalPrice-discount;
     
-} 
+// } 
 
-else if( userAge > 65){
-        const discount= ((totalPrice * 35) / 100);
-        totalPrice=totalPrice-discount;
+// else if( userAge > 65){
+//         const discount= ((totalPrice * 35) / 100);
+//         totalPrice=totalPrice-discount;
         
-} 
+// } 
 
-// ? arrotndiamo a due cifre max dopo la virgola
+// // ? arrotndiamo a due cifre max dopo la virgola
 
-totalPrice= totalPrice.toFixed(2);
-console.log(totalPrice);
+// totalPrice= totalPrice.toFixed(2);
+// console.log(totalPrice);
 
-document.getElementById('costo-biglietto').innerHTML = totalPrice;
+// document.getElementById('costo-biglietto').innerHTML = totalPrice;
