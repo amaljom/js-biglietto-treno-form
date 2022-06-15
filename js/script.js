6919a3786e7cebfe34dcebc89168669fa4a53877
amaljom <amaljom99@gmail.com>
2022-06-15 21:12:58 +0200

    aggiunto un minimo di bootstrap e css

--- js/script.js
+++ js/script.js
@@ -4,9 +4,11 @@ const dataAcquisition = document.querySelector('#data-acquisition');
 dataAcquisition.addEventListener('click', function(){
     // ! faccio in modo che i valori inseriti dall' utente nel form vengano //
     // ! assegnati alle variabili che userò per fare i calcoli //
+    const userName = (document.getElementById('user-name').value);
     const userAge = parseInt(document.getElementById('user-age').value);
     const userDistance = parseInt(document.getElementById('user-km').value);
     console.log(userAge, userDistance);
+    const carrozzaUtente = Math.floor(Math.random() * 10);
 
     const pricePerKm= 0.26;
     let totalPrice= pricePerKm;
@@ -27,6 +29,8 @@ dataAcquisition.addEventListener('click', function(){
 
     totalPrice= totalPrice.toFixed(2);
     console.log(totalPrice);
+    document.getElementById('name-utente').append(userName);
+    document.getElementById('carrozza').append(carrozzaUtente);
     document.getElementById('prezzo').append('€' + totalPrice);
 });
 
